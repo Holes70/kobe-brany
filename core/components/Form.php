daf72f5506acb60595cbbbc5825f72db851cd50b
pholes <patrik.holes@wai.sk>
2021-08-19 11:53:27 +0200

    Form creater upgrade

--- core/components/Form.php
+++ core/components/Form.php
@@ -6,8 +6,8 @@ namespace Form {
     public $con;
 
     private $table_name;
-    private $inputs = array();
-    private $showInputs = array();
+    private $showInputs = NULL;
+    private $requiredInputs = [];
 
     public function __construct($table_name) {
       global $con;
@@ -16,14 +16,28 @@ namespace Form {
       $this->vue_component_loader($this);
 
       $this->table_name = $table_name;
+      $this->requiredInputs = json_encode(
+        ['name', 'price']
+      );
     }
 
+    /**
+     * Form inputs
+     * If $params are simple array push values to indexes
+     * @return $this
+     */
     public function inputs($params = array()) {
-      $this->showInputs = $params;
+      if (array_key_exists(0, $params)) {
+        $this->showInputs = array_combine($params, $params);
+      } else {
+        $this->showInputs = $params;
+      }
+
       return $this;
     }
 
     public function show() {
+
       $inputs = $this->select($this->table_name);
       array_pop($inputs);
       $inputs_keys = array_keys($inputs[0]);
@@ -34,10 +48,10 @@ namespace Form {
 
       if ($this->showInputs != NULL) {
         foreach ($this->showInputs as $key => $val) {
-          $showInputs_fix[$val] = $val;
+          $showInputs_fix[$key] = $val;
         }
       } else {
-        $showInputs_fix = [0 => 'TRUE'];
+        $showInputs_fix = [];
       }
 
       $inputs_fix = json_encode($inputs_fix);
@@ -47,7 +61,8 @@ namespace Form {
         <dia-form :form_params='{
           table_name: \"{$this->table_name}\",
           inputs: $inputs_fix,
-          showInputs: $showInputs_fix
+          showInputs: $showInputs_fix,
+          requiredInputs: {$this->requiredInputs}
         }'></dia-form>";
     }
 
--- web/pages/vue_form.php
+++ web/pages/vue_form.php
@@ -1,6 +1,15 @@
 <?php
 
   $form_users = new Form\Component("users");
+
+  // SHOW just selected inputs
   $form_users->inputs(['first_name', 'last_name']);
+
+  // SHOW selected inputs with custom label names
+  $form_users2 = new Form\Component("users");
+  $form_users2->inputs([
+    'first_name' => 'Krstne meno', 
+    'last_name' => 'Priezvisko'
+  ]);
 
   $form_products = new Form\Component("products");
