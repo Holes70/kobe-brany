389f8d83debd844cf2e1395a8d9ef45c31734df1
Bombarder70 <holespato@gmail.com>
2021-09-01 17:39:00 +0200

    Header to Vue

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -1,10 +1,13 @@
-</div>
-<footer class="page-footer">
-  <div class="footer-copyright">
-    <div class="container center">
-      © <a href="https://www.instagram.com/holesx_x/" style='color:white'>holesx_x</a>
-    </div>
-  </div>
-</footer>
-</body>
-</html>
+<?php
+  global $dia;
+  $dia->html("
+    <footer class='page-footer'>
+      <div class='footer-copyright'>
+        <div class='container center'>
+          © <a href='https://www.instagram.com/holesx_x/' style='color:white'>holesx_x</a>
+        </div>
+      </div>
+    </footer>
+    </body>
+    </html>
+  ");
--- web/includes/header.php
+++ web/includes/header.php
@@ -1,5 +1,10 @@
 <?php
-  global $db;
+  global $db, $dia;
+
+  //$elasticSearch = new Components\Elasticsearch("logstash_test");
+
+  // SET field which will use for search
+  //$elasticSearch->searchFields(['title', 'content']);
 
   $navbar_list = $db->select('dia_navbar');
   array_pop($navbar_list);
@@ -24,66 +29,63 @@
     }
   }
 
+  $html = "
+    <header>
+      <nav class='navbar navbar-expand-lg navbar-light bg-light bg-primary'>
+        <a class='navbar-brand' href='#'>Sequel-D</a>    
+          <div class='collapse navbar-collapse bg-primary'>
+            <ul class='navbar-nav mr-auto'>
+  ";
+
+  foreach ($menuItems as $item) {
+    if (count($item['childrens']) > 0) {
+      $html .= "
+        <li class='nav-item dropdown'>
+        <a 
+          class='nav-link 
+          dropdown-toggle' 
+          href='index.php?page={$item['link']}' 
+          id='navbarDropdown' 
+          role='button' 
+          data-toggle='dropdown' 
+          aria-haspopup='true' 
+          aria-expanded='false'
+        >{$item['name']}</a>
+        <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+      ";
+      foreach ($item['childrens'] as $children) {
+        $html .= "
+          <a 
+            class='dropdown-item' 
+            href='index.php?page={$children['link']}'>
+            {$children['name']}
+          </a>
+        ";
+      }
+      $html .= "</div></li>";
+    } else {
+      $html .="
+        <li class='nav-item'>
+          <a class='nav-link' href='index.php?page={$item['link']}'>{$item['name']}</a>
+        </li>
+      ";
+    }
+  }
+
+  $html .= "
+        </ul>
+      </div>
+      
+    </nav>
+    <div class='second_nav'>
+      <div class='row'>
+        <div class='col-9'>dsadas</div>
+        <div class='col-3'>LOGIN</div>
+      </div>
+    </div>
+    </header>
+  ";
+
+  $dia->html($html)->render();
+
 ?>
-
-<header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light bg-primary">
-    <a class="navbar-brand" href="#">Sequel-D</a>    
-    <div class="collapse navbar-collapse bg-primary">
-      <ul class="navbar-nav mr-auto">
-        <?php
-          $html = "";
-          foreach ($menuItems as $item) {
-            if (count($item['childrens']) > 0) {
-              $html .= "
-                <li class='nav-item dropdown'>
-                <a 
-                  class='nav-link 
-                  dropdown-toggle' 
-                  href='index.php?page={$item['link']}' 
-                  id='navbarDropdown' 
-                  role='button' 
-                  data-toggle='dropdown' 
-                  aria-haspopup='true' 
-                  aria-expanded='false'
-                >{$item['name']}</a>
-                <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
-              ";
-              foreach ($item['childrens'] as $children) {
-                $html .= "
-                  <a 
-                    class='dropdown-item' 
-                    href='index.php?page={$children['link']}'>
-                    {$children['name']}
-                  </a>
-                ";
-              }
-              $html .= "</div></li>";
-            } else {
-              $html .="
-                <li class='nav-item'>
-                  <a class='nav-link' href='index.php?page={$item['link']}'>{$item['name']}</a>
-                </li>
-              ";
-            }
-          }
-
-          echo $html;
-        ?>
-      </ul>
-    </div>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </nav>
-  <div class="second_nav">
-    <div class="row">
-      <div class="col-9">dsadas</div>
-      <div class="col-3">LOGIN</div>
-    </div>
-  </div>
-</header>
-
-
-<div class='container'>
