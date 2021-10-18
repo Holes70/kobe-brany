32c89d3f1f64bf8a4db7feca2a9af02f9983725e
Bombarder70 <holespato@gmail.com>
2021-10-18 21:40:48 +0200

    chat.js

--- web/Website/index.php
+++ web/Website/index.php
@@ -5,15 +5,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src='../../Core/public/js/vue.js'></script>
+  <script src='vue/chartx.js'></script>
   <script type="module" src='index.js'></script>
   <title>Document</title>
     <div id='app'>
-  <test></test>
-  <test></test>
-  <test2></test2>
-  </div>
+      <test></test>
+      <test></test>
+      <test2 juj="x"></test2>
+    </div>
 </head>
 <body>
   
+<script>
+
+</script>
 </body>
 </html>
