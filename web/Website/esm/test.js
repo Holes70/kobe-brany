4012d9f4c7ccb802501c412d3615a0ee30ac9df8
Bombarder70 <holespato@gmail.com>
2021-10-25 22:06:57 +0200

    Delete js files

--- web/Website/esm/test.js
+++
@@ -1,6 +0,0 @@
-import { defineComponent } from './vue.esm.js';
-
-export default defineComponent({
-  name: 'App',
-  template: `<h1>xxx</h1>`
-});
--- web/Website/test/main.js
+++
@@ -1,6 +0,0 @@
-import App from './App.js';
-
-const app = Vue.createApp({
-  render: () => Vue.h(App),
-});
-app.mount('#app');
