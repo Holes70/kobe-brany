6dfdfb61b35dde418fe7095bfb7cc48fdfa3512a
Bombarder70 <holespato@gmail.com>
2021-12-20 17:48:01 +0100

    Produkty

--- Core/vue/components/Gallery.vue
+++ Core/vue/components/Gallery.vue
@@ -1,31 +1,26 @@
 <template>
-  <div class="card mt-5">
-		<div class="card-header">
-			<h4>Galéria</h4>
-		</div>
-   	<div class="container page-top">
-			<div class="row">
-				<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-					<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
-						<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
-					</a>
-				</div>
-				<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-					<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
-						<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
-					</a>
-				</div>
-				<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-					<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
-						<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
-					</a>
-				</div>
-				<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-					<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
-						<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
-					</a>
-				</div>
+	<div class="container page-top">
+		<div class="row">
+			<div class="col-lg-3 col-md-4 col-xs-6 thumb">
+				<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
+					<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
+				</a>
 			</div>
-    </div>
-  </div>
+			<div class="col-lg-3 col-md-4 col-xs-6 thumb">
+				<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
+					<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
+				</a>
+			</div>
+			<div class="col-lg-3 col-md-4 col-xs-6 thumb">
+				<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
+					<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
+				</a>
+			</div>
+			<div class="col-lg-3 col-md-4 col-xs-6 thumb">
+				<a href="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"  class="fancybox" rel="ligthbox">
+					<img src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid"  alt="">
+				</a>
+			</div>
+		</div>
+	</div>
 </template>
--- web/Admin/Pages/produkty.php
+++ web/Admin/Pages/produkty.php
@@ -13,11 +13,18 @@ $produkty->buttons([
   ["name" => "Príslušenstvo", "link" => "prislusenstvo"]
 ]);
 
+$galeria = new \Components\Gallery("products_gallery");
+
 $dia->template(
-  $dia->cardBook(
-    "<div class='card'>
-      ".$produkty->show()."
-    </div>"
-    . "<dia-gallery></dia-gallery>"
-  )
+  $dia->cardBook("
+    <div class='card'>
+      {$produkty->show()}
+    </div>
+    <div class='card mt-5'>
+      <div class='card-header'>
+        <h4>Galéria</h4>
+      </div>
+      {$galeria->show()}
+    </div>
+  ")
 )->render();
