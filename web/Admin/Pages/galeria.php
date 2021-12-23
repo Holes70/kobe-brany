4605176b677d904b28d02338a2879cba25129c8d
Bombarder70 <holespato@gmail.com>
2021-12-23 11:22:24 +0100

    Galeria

--- web/Admin/Pages/galeria.php
+++ web/Admin/Pages/galeria.php
@@ -4,6 +4,7 @@ $productId = $webController->getParam('previous_page_id_form');
 
 $galeria = new \Components\Gallery("products_gallery");
 $galeria->conditions([
+  "select" => "gallery.image as image, products_gallery.id as id",
   "where" => [
     "id_product" => (int) $productId,
   ],
