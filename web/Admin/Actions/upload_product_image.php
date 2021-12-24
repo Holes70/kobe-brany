51659a8c702e7995aeb99c9a192050e6ac8532af
Bombarder70 <holespato@gmail.com>
2021-12-24 17:12:01 +0100

    custom action

--- web/Admin/Actions/upload_product_image.php
+++ web/Admin/Actions/upload_product_image.php
@@ -2,7 +2,22 @@
 
 global $db;
 
-//require();
+require ("{$this->rootDir}/Core/Actions/dia_upload_image.php");
+
+$db->insert_array([
+  'table' => 'gallery',
+  'table_data' => [
+    'image' => $_FILES['name']
+  ]
+]);
+
+$db->insert_array([
+  'table' => 'products_gallery',
+  'table_data' => [
+    'id_product' => 1,
+    'id_product_gallery' => 1
+  ]
+]);
 
 /*$data = $db->request_data();
 
