672fa7c3a3bbe8f498faff10ed68238c02349c92
Bombarder70 <holespato@gmail.com>
2021-12-28 16:49:48 +0100

    diaTable getStrucutreValue update

--- Core/public/js/diaTables.js
+++ Core/public/js/diaTables.js
@@ -1,9 +1,14 @@
 class DiaTables {
-  getStructureValue(colName, structureParam, defaultReturnParam) {
-    if (this.tableStructure[colName]) {
-      if (this.tableStructure[colName][structureParam]) {
-        if (this.tableStructure[colName][structureParam]) return this.tableStructure[colName][structureParam];
-        else return defaultReturnParam;
+  getStructureValue(colName, structureParam, defaultReturnParam, tableStructure, addItallic) {
+    if (tableStructure[colName]) {
+      if (tableStructure[colName][structureParam]) {
+        return tableStructure[colName][structureParam];
+      } else {
+        if (addItallic) {
+          return "<i color='green'>" + defaultReturnParam + "</i>";
+        } else {
+          return defaultReturnParam;
+        }
       }
     }
   }
