8b8b7fc1b9c1f40861e094727d4a4b262e77872e
Bombarder70 <holespato@gmail.com>
2022-01-18 18:07:10 +0100

    upload_gallery_image

--- Core/vue/components/Gallery.vue
+++ Core/vue/components/Gallery.vue
@@ -60,7 +60,7 @@ export default {
   data() {
     return Object.assign(dia, {
       itemEdit: [],
-      uploadAction: "upload_product_image"
+      uploadAction: "upload_gallery_image"
     });
   },
   methods: {
