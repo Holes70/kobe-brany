70d0dbc02a9b7a906d3e3158e5f91132b49c52d2
Bombarder70 <holespato@gmail.com>
2022-01-18 18:20:30 +0100

    upload_product_image

---
+++ web/Admin/Actions/upload_product_image.php
@@ -0,0 +1,19 @@
+<?php
+
+global $db, $webController;
+
+$uploadToDir = "products";
+$colNameFile = array_key_first($_FILES);
+
+require ("{$this->rootDir}/Core/Actions/dia_upload_image.php");
+
+$db->update(
+  tableName: "products",
+  rowId: $webController->getPostParam("id"),
+  data: ['image' => $_FILES[$colNameFile]['name']]
+);
+
+$redirect = $webController->getPostParam("redirect");
+
+header ("Location: {$redirect}");
+exit();
