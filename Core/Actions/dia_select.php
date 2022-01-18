5ae9edf400b3395d50f5481244f4c329e7d29ccd
Bombarder70 <holespato@gmail.com>
2022-01-18 20:33:50 +0100

    pagination in secets retutrn ['data']

--- Core/Actions/dia_select.php
+++ Core/Actions/dia_select.php
@@ -5,10 +5,12 @@
   $data = $db->request_data();
 
   try {
-    $webController->getJson($db->dbSelect(
+    $return['data'] = $db->dbSelect(
       $data->params->tableName,
       (array)$data->params->conditions
-    ));
+    );
+
+    $webController->getJson($return['data']);
   } catch(\Exception $e) {
     echo json_encode([
       "status" => "fail",
---
+++ Core/Actions/dia_select_with_pagination.php
@@ -0,0 +1,38 @@
+<?php
+
+  global $db, $webController;
+
+  $data = $db->request_data();
+
+  $totalCount = $db->dbSelect(
+    tableName: $data->params->tableName, 
+    conditions: ["select" => "count(*) as count"]
+  );
+
+  $pagination = \Core\Bice::pagination(
+    countTotal: reset($totalCount)['count'],
+    currentPage: $data->params->currentPage ?? 1,
+    count: 15
+  );
+
+  $conditions = array_merge(
+    ["limit" => "15 OFFSET {$pagination['offset']}"],
+    (array)$data->params->conditions ?? [],
+  );
+
+  try {
+    $return['data'] = $db->dbSelect(
+      tableName: $data->params->tableName,
+      conditions: $conditions
+    );
+    $return['pages'] = $pagination['pages'];
+
+    $webController->getJson($return);
+  } catch(\Exception $e) {
+    echo json_encode([
+      "status" => "fail",
+      "message" => $e->getMessage()
+    ]);
+  }
+
+?>
