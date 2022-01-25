37f1dbbb5a346fa859c55f285f78192cf750c1d0
Bombarder70 <holespato@gmail.com>
2022-01-25 21:54:33 +0100

    footer header vue files

--- web/Components/Website.vue
+++ web/Components/Website.vue
@@ -12,36 +12,7 @@
 </div>-->
 <!-- ***** Preloader End ***** -->
 
-<!-- ***** Header Area Start ***** -->
-<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <nav class="main-nav">
-          <!-- ***** Logo Start ***** -->
-          <a href="index.html" class="logo">
-            <img src="assets/images/logo.png" alt="Chain App Dev">
-          </a>
-          <!-- ***** Logo End ***** -->
-          <!-- ***** Menu Start ***** -->
-          <ul class="nav">
-            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-            <li class="scroll-to-section"><a href="#services">Services</a></li>
-            <li class="scroll-to-section"><a href="#about">About</a></li>
-            <li class="scroll-to-section"><a href="#pricing">Pricing</a></li>
-            <li class="scroll-to-section"><a href="#newsletter">Newsletter</a></li>
-            <li><div class="gradient-button"><a id="modal_trigger" href="#modal"><i class="fa fa-sign-in-alt"></i> Sign In Now</a></div></li> 
-          </ul>        
-          <a class='menu-trigger'>
-              <span>Menu</span>
-          </a>
-          <!-- ***** Menu End ***** -->
-        </nav>
-      </div>
-    </div>
-  </div>
-</header>
-<!-- ***** Header Area End ***** -->
+<header-com></header-com>
   
 <div id="modal" class="popupContainer" style="display:none;">
   <div class="popupHeader">
@@ -125,5 +96,20 @@
   </section>
 </div>
 
-  
+<footer-com></footer-com>
+
 </template>
+
+<script>
+
+import header from './Header.vue';
+import footer from './Footer.vue';
+
+export default {
+  components: {
+    'header-com': header,
+    'footer-com': footer
+  }
+}
+
+</script>
