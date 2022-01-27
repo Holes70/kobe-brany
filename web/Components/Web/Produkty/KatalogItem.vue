b5378e4e58f7dbe973a98c3cd97e6fc4ecd188fd
Bombarder70 <holespato@gmail.com>
2022-01-27 22:56:18 +0100

    KtalogItem

--- web/Components/Web/Produkty/KatalogItem.vue
+++ web/Components/Web/Produkty/KatalogItem.vue
@@ -1,11 +1,11 @@
 <template>
-  <div :id="'katalog-item-' + item.id" class="col-lg-3 mb-4">
+  <div :id="'katalog-item-' + item.id" class="item-hover col-lg-3 mb-4">
     <div class="service-item first-service">
       <div class="katalog-item-img">
         <img src="assets/images/brana-1.jpg"/>
       </div>
       <h4>{{ item.name }}</h4>
-      <h3>{{ item.price }}€</h3>
+      <h5>{{ item.price }}€</h3>
       <p>{{ item.description }}</p>
       <div class="text-button">
         <a href="#">Do košíka<i class="fa fa-arrow-right"></i></a>
