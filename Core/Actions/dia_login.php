3c0b50fbdbf40b94c033d82fb24ab14bd5a7631d
Bombarder70 <holespato@gmail.com>
2022-02-03 10:58:26 +0100

    dia_Login

--- Core/Actions/dia_login.php
+++ Core/Actions/dia_login.php
@@ -3,19 +3,32 @@
   global $db, $userController, $webController;
 
   $data = $webController->getPostParams();
-  print_r($data); exit();
+
   try {
+    $selectUserIfExists = [];
     $selectUserIfExists = $db->dbSelect(
       $data['tableName'],
       [
         "where" => [
-          'email' => $data['email']
+          "email" => $data['email']
         ]
       ]
     );
 
+    $selectUserIfExists = reset($selectUserIfExists);
+
+    if (empty($selectUserIfExists)) {
+      throw new \Exception("User doesnt exists");
+    }
+
+    if (!password_verify($data['password'], $selectUserIfExists['password'])) {
+      throw new \Exception("Password not valid");
+    }
+
     $userController->setUser($selectUserIfExists);
     $userController->setLogged();
+
+    $webController->redirect("profile");
   } catch(\Exception $e) {
     echo json_encode([
       "status" => "fail",
