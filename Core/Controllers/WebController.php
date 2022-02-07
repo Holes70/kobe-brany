80d22371327304f82e01a10a0bf975a28fa54b09
Bombarder70 <holespato@gmail.com>
2022-02-07 11:32:14 +0100

    docs

--- Core/Controllers/WebController.php
+++ Core/Controllers/WebController.php
@@ -4,14 +4,26 @@ namespace Core\Controllers {
 
   class WebController extends \Core\Dia {
 
+    /**
+     * SET memory breacrumbs value
+     * @return void
+     */
     public static function setMemory(string $url) {
       $_SESSION['memory'][$url] = $url;
     }
 
+    /**
+     * GET memory breacrumbs values
+     * @return array
+     */
     public static function getAllMemory() {
       return isset($_SESSION['memory']) ? $_SESSION['memory'] : [];
     }
 
+    /**
+     * SET url for memory
+     * @return void
+     */
     public static function setUrlForMemory(string $url) {
       if (!isset($_SESSION['memory'][$url])) {
         self::setMemory($url);
@@ -20,18 +32,37 @@ namespace Core\Controllers {
       self::setCurrentPage();
     }
 
+    /**
+     * SET current web page
+     * if param webPage empty
+     * default web from config home
+     * @return void
+     */
     public static function setCurrentPage() {
-      $_SESSION['currentPage'] = isset($_GET['webPage']) ? $_GET['webPage'] : "home";
+      global $config;
+      $_SESSION['currentPage'] = isset($_GET['webPage']) ? $_GET['webPage'] : $config['web']['home'];
     }
 
+    /**
+     * GET current web page session
+     * @return string
+     */
     public static function getCurrentPageSession() {
       return isset($_SESSION['currentPage']) ? $_SESSION['currentPage'] : "";
     }
 
+    /**
+     * GET current web page
+     * @return string
+     */
     public static function getCurrentPage() {
       return isset($_GET['webPage']) ? $_GET['webPage'] : self::getCurrentPageSession();
     }
 
+    /**
+     * UNSET memory breadcrumbs
+     * @return array
+     */
     public static function destroyMemory() {
       unset($_SESSION['memory']);
       
@@ -40,6 +71,10 @@ namespace Core\Controllers {
       return self::getAllMemory();
     }
 
+    /**
+     * GET web page dir 
+     * @return string
+     */
     public static function getPage() {
       global $config;
 
@@ -52,6 +87,10 @@ namespace Core\Controllers {
       return $pageToInclude;
     }
 
+    /**
+     * GET JSON from array
+     * @return string
+     */
     public static function getJson(array $data) {
       $dataToArray = $data;
 
@@ -70,10 +109,18 @@ namespace Core\Controllers {
       echo json_encode($dataToArray);
     }
 
+    /**
+     * CHECK $_GET param if exists
+     * @return bool
+     */
     public static function checkParamIfExists(string $param) {
       return isset($_GET[$param]) ? true : false;
     }
 
+    /**
+     * GET $_GET param
+     * @return string
+     */
     public static function getParam(string $param) {
       return isset($_GET[$param]) ? $_GET[$param] : "";
     }
