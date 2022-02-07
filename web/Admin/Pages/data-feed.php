42ed187a6e68ebe55aca0165daa61e46ec3f1262
Bombarder70 <holespato@gmail.com>
2022-02-07 11:39:18 +0100

    order_type restore

--- web/Admin/Pages/data-feed.php
+++ web/Admin/Pages/data-feed.php
@@ -4,7 +4,7 @@
 //include(__DIR__ . '/data-feed/orders.php');
 //include(__DIR__ . '/data-feed/products.php');
 //include(__DIR__ . '/data-feed/products_accessories.php');
-//include(__DIR__ . '/data-feed/order_type.php')
+//include(__DIR__ . '/data-feed/order_type.php');
 //include(__DIR__ . '/data-feed/customers.php');
 //include(__DIR__ . '/data-feed/gallery.php');
 //include(__DIR__ . '/data-feed/products_gallery.php');
