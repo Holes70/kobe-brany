a448bb5d6f3552ee3ef646f2852d57a5eb6207af
Bombarder70 <holespato@gmail.com>
2022-02-10 13:31:23 +0100

    mesagges

--- Core/Components/Vue/Messages.vue
+++ Core/Components/Vue/Messages.vue
@@ -1,6 +1,13 @@
 <template>
-  <div v-for="itemData in data" :key="itemData" class="card">
-    xxx
+  <div v-for="itemData in data" :key="itemData" class="card p-1 m-1">
+    <div class="row">
+      <div v-for="item in itemData" :key="item.id" class="col">
+        {{ item }}
+      </div>
+      <div class="col">
+        <i class="far fa-trash-alt icon-danger"></i>
+      </div>
+    </div>
   </div>
 </template>
 
