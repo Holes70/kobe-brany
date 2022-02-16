8e9b18bf96c59fc9bf4fa1c362e9d5a495528ff1
Bombarder70 <holespato@gmail.com>
2022-02-16 14:44:43 +0100

    produkty

--- web/Admin/Pages/produkty.php
+++ web/Admin/Pages/produkty.php
@@ -20,9 +20,65 @@ $produkty->buttons([
 $pridat = new Row("products");
 $pridat->title("Produkty");
 
+$produktyQuery = $db->dbSelect(
+  tableName: "products",
+  conditions: [
+    "where" => [
+      "type" => 1
+    ]
+  ]
+);
+
+$pocetProduktov = count($produktyQuery);
+
+$pocetProduktovNedostupnych = 0;
+
+foreach ($produktyQuery as $produkt) {
+  if (!$produkt['available']) {
+    $pocetProduktovNedostupnych++;
+  }
+}
+
 $dia->template("
   {$pridat->show()}
-  ".$dia->card(
-    $produkty->show()
-  )."
+  <div class='row'>
+    <div class='col-9'>
+      ".$dia->card($produkty->show())."
+    </div>
+    <div class='col-3'>
+      <div class='card'>
+        <div class='card-body bg-primary text-center'>
+          <h5 class='color-white'>Prehľad produktov</h5>
+        </div>
+      </div>
+      <div class='card mt-2'>
+        <div class='card-body'>
+          <i class='fas fa-archive'></i> Spolu produktov: <span class='badge badge-primary bg-secondary badge-pill'>{$pocetProduktov}</span>
+        </div>
+      </div>
+      <div class='card mt-2'>
+        <div class='card-body'>
+          <i class='fas fa-archive'></i> Počet nedostupných: <span class='badge badge-primary bg-danger badge-pill'>{$pocetProduktovNedostupnych}</span>
+        </div>
+      </div>
+      <div class='mt-5'>
+        <dia-button :params='{
+          button: \"danger\",
+          title: \"Exportovať do CSV\",
+          icon: \"fas fa-file-csv\",
+          modalComponentToRender: \"dia-messages\",
+          modalComponentToRenderParams: {
+            tableName: \"dia_notifications\",
+            data: [],
+            conditions: [],
+            customActions: {
+              loadData: \"dia_get_notifications\",
+              editUrl: \"url\",
+              afterEdit: \"dia_update_notifications_viewed\"
+            }
+          }
+        }'></dia-button>
+      </div>
+    </div>
+  </div>
 ")->render();
