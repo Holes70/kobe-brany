240f4c20bb76a526b55a0af9e335e01408f7010a
Bombarder70 <holespato@gmail.com>
2022-02-28 12:33:28 +0100

    ....

---
+++ web/Admin/Pages/eshop.php
@@ -0,0 +1,12 @@
+<?php
+
+$dia->template(
+  $dia->card("
+    <h3 class='card-title color-dark'>Správa navigačného menu stránky</h3>
+    <p class='card-text'>Správa navigačného menu stránky</p>
+    <a href='nastavenia-menu?previous_page=systemove-nastavenia' class='btn btn-dark'>Spravovať</a>
+  ")
+  ."<div class='mt-5'></div>"
+)->render();
+
+?>
--- web/Admin/Pages/systemove-nastavenia.php
+++ web/Admin/Pages/systemove-nastavenia.php
@@ -2,12 +2,6 @@
 
 $dia->template(
   $dia->card("
-    <h3 class='card-title color-dark'>Správa navigačného menu stránky</h3>
-    <p class='card-text'>Správa navigačného menu stránky</p>
-    <a href='nastavenia-menu?previous_page=systemove-nastavenia' class='btn btn-dark'>Spravovať</a>
-  ")
-  ."<div class='mt-5'></div>".
-  $dia->card("
     <h3 class='card-title color-red'>Správa admin menu</h3>
     <p class='card-text'>Upravovanie navigačného menu pre administrácie</p>
     <a href='nastavenia-navigacia?previous_page=systemove-nastavenia' class='btn btn-danger'>Spravovať</a>
