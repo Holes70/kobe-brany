dd21b91d15d3472bf29561611b6c22f7a2a2ed18
Bombarder70 <holespato@gmail.com>
2022-03-01 21:08:40 +0100

    itemId

--- web/Components/Detail.vue
+++ web/Components/Detail.vue
@@ -1,5 +1,13 @@
 <template>
 <div class="main-banner wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s"></div>
     DETAIL
-      {{ this.$route.params.username }}
+      {{ this.$route.params.itemId }}
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$route.params.item );
+  }
+}
+</script>
