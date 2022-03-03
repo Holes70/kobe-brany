0ee2bc136727b225c9cfc8bea0186ddce79aad03
Bombarder70 <holespato@gmail.com>
2022-03-03 13:12:41 +0100

    Detail

--- web/Components/Detail.vue
+++ web/Components/Detail.vue
@@ -3,6 +3,23 @@
     DETAIL
       <h1>{{ this.$route.params.itemId }}</h1>
       {{ data.name }}
+
+    <div class="container">
+      <div :id="'katalog-item-' + data.id">
+        <div class="service-item first-service">
+          <div class="katalog-item-img">
+            <img :src="'http://localhost/holes/kobe-brany/files/products/' + data.image"/>
+          </div>
+          <h4>{{ data.name }}</h4>
+          <h5>{{ data.price }} €</h5>
+          <p>{{ data.description }}</p>
+
+          <div class="gold-button">
+            <button>Pridať do košíka</button>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
