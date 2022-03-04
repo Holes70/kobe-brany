69541d4abf273047dcfacfe6120da986d096af30
Bombarder70 <holespato@gmail.com>
2022-03-04 09:17:56 +0100

    Products

--- web/Admin/Pages/data-feed/products.php
+++ web/Admin/Pages/data-feed/products.php
@@ -3,6 +3,7 @@
 for ($i=1;$i<=100;$i++) {
   $type = rand(1,2);
   $name = $type == 1 ? "Produkt" : "Príslušenstvo";
+  $price = rand(0,1000);
 
   $db->insert_array([
     'table' => 'products',
@@ -11,11 +12,14 @@ for ($i=1;$i<=100;$i++) {
       'image' => "product_" . rand(1, 3) . ".jpg",
       'name' => $name . '-' . $i,	
       'description' => 'Popis', 	
-      'price' => rand(0,1000),
+      'price' => $price,
+      'vat' => 20,
+      'price_without_vat' => round(($price / 1.20), 2),
       'available' => rand(0,1),
       'count' => rand(0,15),
       'type' => $type,
-      'state' => rand(1, 3)
+      'state' => rand(1, 3),
+      'discount' => (rand(1, 5) == 3) ? 20 : null
     ]
   ]);
 }
