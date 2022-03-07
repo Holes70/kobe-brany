ac8cf3f9e8b3cf57de2b09c60325597be3e75477
Bombarder70 <holespato@gmail.com>
2022-03-07 18:04:28 +0100

    Kosik

--- web/Components/Kosik.vue
+++ web/Components/Kosik.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-banner wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s"></div>
     <div id="kosik" class="services section">
-      <div class="container m-5 p-3">
+      <div class="container">
         <table class="table table-striped">
           <tbody>
             <tr>
@@ -27,3 +27,33 @@
       </div>
     </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    loadData() {
+      axios.post('Admin/index.php?action=dia_select', {
+        params: {
+          tableName: "carts",
+          conditions: {
+            where: {
+              "carts.id": 1
+            },
+            join: {
+              carts_products: {
+                1: "id",
+                2: "id_cart"
+              }
+            }
+          }
+        }
+      }).then((res) => {
+        console.log(res);
+      })
+    },
+    beforeMount() {
+      this.loadData();
+    }
+  }
+}
+</script>
