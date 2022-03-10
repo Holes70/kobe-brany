d370f1c5cc18e6a91087e8ce91de694915e44e2e
Bombarder70 <holespato@gmail.com>
2022-03-10 15:59:45 +0100

    Empty kosik

--- web/Components/Kosik.vue
+++ web/Components/Kosik.vue
@@ -2,39 +2,49 @@
   <div class="main-banner wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s"></div>
     <div id="kosik" class="services">
       <div class="section container">
-        <table class="table">
-          <tbody>
-            <tr v-for="item in data" :key="item.id">
-              <td>{{ item.name }}</td>
-              <td>{{ item.price_without_vat }}€</td>
-              <td>{{ item.vat }}%</td>
-              <td>{{ item.price }}€</td>
-              <td>
-                <button 
-                  @click="deleteItem(item.idCartProduct)"
-                  class="btn btn-danger"
-                >
-                  <i class="fas fa-times"></i>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div v-if="Object.keys(data).legth > 0">
+          <table class="table">
+            <tbody>
+              <tr v-for="item in data" :key="item.id">
+                <td>{{ item.name }}</td>
+                <td>{{ item.price_without_vat }}€</td>
+                <td>{{ item.vat }}%</td>
+                <td>{{ item.price }}€</td>
+                <td>
+                  <button 
+                    @click="deleteItem(item.idCartProduct)"
+                    class="btn btn-danger"
+                  >
+                    <i class="fas fa-times"></i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
-        <div class="row">
-          <div class="col-6">
-            <div class="box-item">
-              <h4>
-                <a href="#">400</a>
-              </h4>
-              <p>Spolu</p>
+          <div class="row">
+            <div class="col-6">
+              <div class="box-item">
+                <h4>
+                  <a href="#">400</a>
+                </h4>
+                <p>Spolu</p>
+              </div>
+            </div>
+            <div class="col-6">
+              <router-link :to="{name: 'Objednavka'}">
+                <div class="gradient-button"><a href="#">Prejsť k objednávke</a></div>
+              </router-link>
             </div>
           </div>
-          <div class="col-6">
-            <router-link :to="{name: 'Objednavka'}">
-              <div class="gradient-button"><a href="#">Prejsť k objednávke</a></div>
-            </router-link>
-          </div>
+        </div>
+        <div v-else class="mt-5 text-center">
+          <h1>Váš nákupný košík je prázdny</h1>
+          <img 
+            src="http://localhost/holes/kobe-brany/files/empty-cart.jpg" 
+            alt="prazdny_kosik"
+            style="height:250px;width:365px"
+          >
         </div>
       </div>
     </div>
